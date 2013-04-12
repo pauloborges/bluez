199eeee4cd21fa2abf83eea0c55b0ee5ec66f53c
Anderson Lizardo <anderson.lizardo@openbossa.org>
2013-04-12 16:27:04 -0400

    attrib: Run callback with the partial result of characteristic discovery
    Callbacks should check for status equal to ATT_ECODE_ATTR_NOT_FOUND to
    ensure that the characteristic discovery has finished.

--- attrib/gatt.c
+++ attrib/gatt.c
@@ -502,7 +502,8 @@ static void char_discovered_cb(guint8 status, const guint8 *ipdu, guint16 iplen,
 {
 	struct discover_char *dc = user_data;
 	struct att_data_list *list;
-	unsigned int i, err = ATT_ECODE_ATTR_NOT_FOUND;
+	unsigned int i, err;
+	bool continue_discovery;
 	uint16_t last = 0;
 
 	if (status) {
@@ -548,6 +549,24 @@ static void char_discovered_cb(guint8 status, const guint8 *ipdu, guint16 iplen,
 
 	att_data_list_free(list);
 
+	/* From the Core spec: "It is permitted to end the sub-procedure early
+	 * if a desired characteristic is found prior to discovering all the
+	 * characteristics of the specified service supported on the server."
+	 *
+	 * In other words, this callback will receive the partial list
+	 * of discovered characteristics, and if it returns false, the
+	 * procedure is interrupted.
+	 */
+	if (dc->characteristics != NULL) {
+		continue_discovery = dc->cb(status, dc->characteristics,
+								dc->user_data);
+		g_slist_free_full(dc->characteristics, g_free);
+		dc->characteristics = NULL;
+
+		if (!continue_discovery)
+			goto data_free;
+	}
+
 	if (last != 0 && (last + 1 < dc->end)) {
 		bt_uuid_t uuid;
 		guint16 oplen;
@@ -570,10 +589,12 @@ static void char_discovered_cb(guint8 status, const guint8 *ipdu, guint16 iplen,
 		return;
 	}
 
-done:
-	err = (dc->characteristics ? 0 : err);
+	/* Procedure has finished */
+	err = ATT_ECODE_ATTR_NOT_FOUND;
 
+done:
 	dc->cb(err, dc->characteristics, dc->user_data);
+data_free:
 	discover_char_free(dc);
 }
 
