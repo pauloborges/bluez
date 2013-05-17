580f22ee10ac6f27127867b3615db28226b698da
Vinicius Costa Gomes <vinicius.gomes@openbossa.org>
2013-05-17 18:57:27 -0300

    gatt: Keep track of application implementing GATT services

--- src/gatt.c
+++ src/gatt.c
@@ -26,19 +26,106 @@
 #endif
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #include <glib.h>
 #include <dbus/dbus.h>
 #include <gdbus/gdbus.h>
 
 #include "dbus-common.h"
+#include "log.h"
+#include "error.h"
 
 #include "gatt.h"
+
+struct application {
+	char *owner;
+	GSList *services;
+	unsigned int watch;
+};
+
+static GSList *applications = NULL;
+
+static void destroy_application(void *data)
+{
+	struct application *app = data;
+
+	DBG("app %p", app);
+
+	g_free(app->owner);
+	g_slist_free_full(app->services, g_free);
+
+	if (app->watch > 0)
+		g_dbus_remove_watch(btd_get_dbus_connection(), app->watch);
+
+	applications = g_slist_remove(applications, app);
+
+	g_free(app);
+}
+
+static void application_disconnected(DBusConnection *conn, void *user_data)
+{
+	destroy_application(user_data);
+}
+
+static struct application *new_application(const char *sender)
+{
+	struct application *app;
+
+	app = g_new0(struct application, 1);
+
+	app->watch = g_dbus_add_disconnect_watch(btd_get_dbus_connection(),
+				sender, application_disconnected, app, NULL);
+	if (app->watch == 0) {
+		g_free(app);
+		return NULL;
+	}
+
+	app->owner = g_strdup(sender);
+
+	applications = g_slist_prepend(applications, app);
+
+	return app;
+}
 
 static DBusMessage *register_services(DBusConnection *conn,
 					DBusMessage *msg, void *user_data)
 {
-	return dbus_message_new_method_return(msg);
+	struct application *app;
+	DBusMessageIter args, iter;
+
+	DBG("Registering GATT Service");
+
+	if (dbus_message_iter_init(msg, &args) == false)
+		goto invalid;
+
+	if (dbus_message_iter_get_arg_type(&args) != DBUS_TYPE_ARRAY)
+		goto invalid;
+
+	app = new_application(dbus_message_get_sender(msg));
+	if (app == NULL)
+		return btd_error_failed(msg, "Not enough resources");
+
+	DBG("new app %p", app);
+
+	dbus_message_iter_recurse(&args, &iter);
+
+	while (dbus_message_iter_get_arg_type(&iter) == DBUS_TYPE_OBJECT_PATH) {
+		const char *path;
+
+		dbus_message_iter_get_basic(&iter, &path);
+
+		app->services = g_slist_append(app->services, g_strdup(path));
+
+		DBG("path %s", path);
+
+		dbus_message_iter_next(&iter);
+	}
+
+	return g_dbus_create_reply(msg, DBUS_TYPE_INVALID);
+
+invalid:
+	return btd_error_invalid_args(msg);
 }
 
 static DBusMessage *unregister_services(DBusConnection *conn,
