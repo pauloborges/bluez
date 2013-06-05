589abbd7b16aee509c25f5973dc0f2e882bb9946
Claudio Takahasi <claudio.takahasi@openbossa.org>
2013-06-05 11:18:41 -0300

    proximity: Add Immediate Alert Service
    This patch adds the service and characteristic declaration of the
    Immediate Alert Service using the new attribute API.

--- profiles/proximity/ias.c
+++ profiles/proximity/ias.c
@@ -24,9 +24,50 @@
 #include <config.h>
 #endif
 
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include <glib.h>
+
+#include "lib/uuid.h"
+#include "attrib/att.h"
+
+#include "adapter.h"
+#include "device.h"
+#include "log.h"
+
+#include "gatt.h"
 #include "ias.h"
+
+#define IMMEDIATE_ALERT_SVC_UUID	0x1802
+#define ALERT_LEVEL_CHR_UUID		0x2A06
+
+static struct btd_attribute *ias = NULL;
+static struct btd_attribute *ial = NULL;
+
+static void write_ial_cb(struct btd_device *device,
+			uint8_t *value, size_t len, uint16_t offset,
+			btd_attr_write_result_t result, void *user_data)
+{
+	DBG("Immediate Alert Level: 0x%02x", value[0]);
+
+	/*
+	 * For Write Without Response "result" callback doesn't
+	 * need to called. Confirmation is not applied.
+	 */
+}
 
 void ias_init(void)
 {
+	bt_uuid_t uuid;
 
+	/* Immediate Alert Primary Service declaration */
+	bt_uuid16_create(&uuid, IMMEDIATE_ALERT_SVC_UUID);
+	ias = btd_gatt_add_service(&uuid, true);
+
+	/* Declaration and Value: Alert Level */
+	bt_uuid16_create(&uuid, ALERT_LEVEL_CHR_UUID);
+	ial = btd_gatt_add_char(&uuid, ATT_CHAR_PROPER_WRITE_WITHOUT_RESP,
+							NULL, write_ial_cb);
 }
