dc3e2879623b0cd1b328f9e3fe958c1700290130
Claudio Takahasi <claudio.takahasi@openbossa.org>
2013-06-06 10:59:20 -0300

    gatt: Enable new server if experimental is set
    The current GATT server conflicts with the new Attribute Server that is
    being developed. This patch enables the new Attribute Server is
    Experimental option is informed.

--- src/gatt.c
+++ src/gatt.c
@@ -1031,6 +1031,9 @@ void btd_gatt_service_manager_init(void)
 {
 	GError *gerr = NULL;
 
+	if (!(g_dbus_get_flags() & G_DBUS_FLAG_ENABLE_EXPERIMENTAL))
+		return;
+
 	DBG("Starting GATT server");
 
 	bredr_io = bt_io_listen(connect_event, NULL, NULL, NULL, &gerr,
