988662b89828fb3544c3b8e19847867f3417ec9b
Paulo Borges <paulo.borges@openbossa.org>
2013-06-11 18:06:12 -0300

    gatt/gap: Read Device Name Characteristic
    This commit adds logic to discover Device Name Characteristic from
    GAP Service, read it and set the btd_device name using the response.

--- profiles/gatt/gatt.c
+++ profiles/gatt/gatt.c
@@ -24,23 +24,95 @@
 #include <config.h>
 #endif
 
+#include <glib.h>
+
 #include "lib/uuid.h"
 #include "plugin.h"
 #include "adapter.h"
+#include "device.h"
 #include "profile.h"
 #include "service.h"
+#include "gatt.h"
 #include "log.h"
+
+static GSList *devices = NULL;
+
+static void read_device_name_chr_cb(int err, uint8_t *value, size_t len,
+							void *user_data)
+{
+	struct btd_device *device = user_data;
+
+	if (err) {
+		error("Error reading <<Device Name>>: %d", err);
+		return;
+	}
+
+	value[len - 1] = '\0';
+	device_set_name(device, (char *) value);
+}
+
+static void read_device_name_chr(struct btd_device *device, GList *attrib_db,
+						struct btd_attribute *gap)
+{
+	struct btd_attribute *chr, *chr_value;
+	bt_uuid_t uuid;
+	GSList *list;
+
+	bt_uuid16_create(&uuid, GATT_CHARAC_DEVICE_NAME);
+	list = btd_gatt_get_chars_decl(attrib_db, gap, &uuid);
+	if (!list) {
+		error("<<Device Name>> characteristic is mandatory");
+		return;
+	}
+
+	chr = list->data;
+
+	chr_value = btd_gatt_get_char_value(attrib_db, chr);
+	btd_gatt_read_attribute(device, chr_value, read_device_name_chr_cb,
+								device);
+}
+
+static void find_gap(struct btd_device *device)
+{
+	struct btd_attribute *gap;
+	GList *attrib_db;
+	bt_uuid_t uuid;
+	GSList *list;
+
+	attrib_db = btd_device_get_attribute_database(device);
+
+	bt_uuid16_create(&uuid, GENERIC_ACCESS_PROFILE_ID);
+	list = btd_gatt_get_services(attrib_db, &uuid);
+	if (!list) {
+		error("<<GAP Service>> is mandatory");
+		return;
+	}
+
+	gap = list->data;
+
+	read_device_name_chr(device, attrib_db, gap);
+}
 
 static int gatt_driver_probe(struct btd_service *service)
 {
+	struct btd_device *device = btd_service_get_device(service);
+
 	DBG("Probing device");
+
+	find_gap(device);
+
+	devices = g_slist_append(devices, device);
 
 	return 0;
 }
 
 static void gatt_driver_remove(struct btd_service *service)
 {
+	struct btd_device *device = btd_service_get_device(service);
+
 	DBG("Removing device");
+
+	devices = g_slist_remove(devices, device);
 }
 
 static struct btd_profile gatt_profile = {
