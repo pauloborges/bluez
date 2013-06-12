5f6d01a3e34a1d31cece298cebf04fd1c8e03795
Paulo Borges <paulo.borges@openbossa.org>
2013-06-12 15:30:53 -0300

    WIP gatt/gap: Handle Service Changed indication

--- profiles/gatt/gatt.c
+++ profiles/gatt/gatt.c
@@ -36,7 +36,30 @@
 #include "attrib/att.h"
 #include "log.h"
 
+struct gatt_device {
+	struct btd_device *dvc;
+	struct btd_attribute *svc_chd_chr;
+	unsigned int svc_chd_notif_id;
+};
+
 static GSList *devices = NULL;
+
+static void gatt_device_destroy(struct gatt_device *device)
+{
+	if (device->svc_chd_notif_id)
+		btd_gatt_remove_notifier(device->dvc, device->svc_chd_chr,
+						device->svc_chd_notif_id);
+
+	g_free(device);
+}
+
+static gint gatt_device_cmp(gconstpointer a, gconstpointer b)
+{
+	const struct gatt_device *device = a;
+	const struct btd_device *dvc = b;
+
+	return device->dvc != dvc;
+}
 
 static void read_device_name_chr_cb(int err, uint8_t *value, size_t len,
 							void *user_data)
@@ -52,7 +75,7 @@ static void read_device_name_chr_cb(int err, uint8_t *value, size_t len,
 	device_set_name(device, (char *) value);
 }
 
-static void read_device_name_chr(struct btd_device *device, GList *attrib_db,
+static void read_device_name_chr(struct gatt_device *device, GList *attrib_db,
 						struct btd_attribute *gap)
 {
 	struct btd_attribute *chr, *chr_value;
@@ -69,14 +92,14 @@ static void read_device_name_chr(struct btd_device *device, GList *attrib_db,
 	chr = list->data;
 
 	chr_value = btd_gatt_get_char_value(attrib_db, chr);
-	btd_gatt_read_attribute(device, chr_value, read_device_name_chr_cb,
-								device);
+	btd_gatt_read_attribute(device->dvc, chr_value,
+					read_device_name_chr_cb, device);
 }
 
 static void read_appearance_chr_cb(int err, uint8_t *value, size_t len,
 							void *user_data)
 {
-	struct btd_device *device = user_data;
+	struct gatt_device *device = user_data;
 	uint16_t appearance;
 
 	if (err) {
@@ -85,10 +108,10 @@ static void read_appearance_chr_cb(int err, uint8_t *value, size_t len,
 	}
 
 	appearance = att_get_u16(value);
-	device_set_appearance(device, appearance);
+	device_set_appearance(device->dvc, appearance);
 }
 
-static void read_appearance_chr(struct btd_device *device, GList *attrib_db,
+static void read_appearance_chr(struct gatt_device *device, GList *attrib_db,
 						struct btd_attribute *gap)
 {
 	struct btd_attribute *chr, *chr_value;
@@ -105,18 +128,18 @@ static void read_appearance_chr(struct btd_device *device, GList *attrib_db,
 	chr = list->data;
 
 	chr_value = btd_gatt_get_char_value(attrib_db, chr);
-	btd_gatt_read_attribute(device, chr_value, read_appearance_chr_cb,
+	btd_gatt_read_attribute(device->dvc, chr_value, read_appearance_chr_cb,
 								device);
 }
 
-static void find_gap(struct btd_device *device)
+static void find_gap(struct gatt_device *device)
 {
 	struct btd_attribute *gap;
 	GList *attrib_db;
 	bt_uuid_t uuid;
 	GSList *list;
 
-	attrib_db = btd_device_get_attribute_database(device);
+	attrib_db = btd_device_get_attribute_database(device->dvc);
 
 	bt_uuid16_create(&uuid, GENERIC_ACCESS_PROFILE_ID);
 	list = btd_gatt_get_services(attrib_db, &uuid);
@@ -131,13 +154,72 @@ static void find_gap(struct btd_device *device)
 	read_appearance_chr(device, attrib_db, gap);
 }
 
+static bool service_changed_chr_changed_cb(uint8_t *value, size_t len,
+							void *user_data)
+{
+	/* TODO Handle Service Changed indication */
+	DBG("<<Service Changed>> INDICATION received");
+
+	return true;
+}
+
+static void read_service_changed_chr(struct gatt_device *device,
+						GList *attrib_db,
+						struct btd_attribute *gatt)
+{
+	struct btd_attribute *dsc;
+	bt_uuid_t uuid;
+	GSList *list;
+
+	bt_uuid16_create(&uuid, GATT_CHARAC_SERVICE_CHANGED);
+	list = btd_gatt_get_chars_decl(attrib_db, gatt, &uuid);
+	if (!list) {
+		error("<<Service Changed>> characteristic is mandatory");
+		return;
+	}
+
+	device->svc_chd_chr = list->data;
+
+	bt_uuid16_create(&uuid, GATT_CLIENT_CHARAC_CFG_UUID);
+	dsc = btd_gatt_get_char_desc(attrib_db, device->svc_chd_chr, &uuid);
+
+	device->svc_chd_notif_id = btd_gatt_add_notifier(device->dvc, dsc,
+				service_changed_chr_changed_cb, device);
+}
+
+static void find_gatt(struct gatt_device *device)
+{
+	struct btd_attribute *gatt;
+	GList *attrib_db;
+	bt_uuid_t uuid;
+	GSList *list;
+
+	attrib_db = btd_device_get_attribute_database(device->dvc);
+
+	bt_uuid16_create(&uuid, GENERIC_ATTRIB_PROFILE_ID);
+	list = btd_gatt_get_services(attrib_db, &uuid);
+	if (!list) {
+		error("<<GATT Service>> is mandatory");
+		return;
+	}
+
+	gatt = list->data;
+
+	read_service_changed_chr(device, attrib_db, gatt);
+}
+
 static int gatt_driver_probe(struct btd_service *service)
 {
-	struct btd_device *device = btd_service_get_device(service);
+	struct btd_device *dvc = btd_service_get_device(service);
+	struct gatt_device *device;
 
 	DBG("Probing device");
 
+	device = g_new0(struct gatt_device, 1);
+	device->dvc = dvc;
+
 	find_gap(device);
+	find_gatt(device);
 
 	devices = g_slist_append(devices, device);
 
@@ -146,11 +228,19 @@ static int gatt_driver_probe(struct btd_service *service)
 
 static void gatt_driver_remove(struct btd_service *service)
 {
-	struct btd_device *device = btd_service_get_device(service);
+	struct btd_device *dvc = btd_service_get_device(service);
+	struct gatt_device *device;
+	GSList *list;
 
 	DBG("Removing device");
 
+	list = g_slist_find_custom(devices, dvc, gatt_device_cmp);
+	if (list == NULL)
+		return;
+
+	device = list->data;
 	devices = g_slist_remove(devices, device);
+	gatt_device_destroy(device);
 }
 
 static struct btd_profile gatt_profile = {
