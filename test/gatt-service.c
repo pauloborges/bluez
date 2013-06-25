72b855f23197b1da6e5f30e50c74dfe5cddf8551
Eder Ruiz Maria <eder.ruiz@openbossa.org>
2013-06-25 13:06:23 -0400

    gatt-service: Fix iterator initialization at ReadValue DBus method

--- test/gatt-service.c
+++ test/gatt-service.c
@@ -240,7 +240,7 @@ static DBusMessage *chr_read_value(DBusConnection *conn, DBusMessage *msg,
 
 	reply = dbus_message_new_method_return(msg);
 
-	dbus_message_iter_init(reply, &iter);
+	dbus_message_iter_init_append(reply, &iter);
 
 	dbus_message_iter_open_container(&iter, DBUS_TYPE_ARRAY,
 					DBUS_TYPE_BYTE_AS_STRING, &array);
