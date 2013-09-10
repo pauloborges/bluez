27b60ee62b9e1c69089b9d8b14db81877eb30d4d
Claudio Takahasi <claudio.takahasi@openbossa.org>
2013-09-10 13:45:38 -0300

    scan param: Convert to the new GATT API
    This patch converts the Scan Parameter plugin to use the new GATT API.
    Attribute is being discovered by the core, Scan Parameter plugin should
    use the helper functions to read and write the wanted attributes.

--- profiles/scanparam/scan.c
+++ profiles/scanparam/scan.c
@@ -37,235 +37,155 @@
 #include "profile.h"
 #include "service.h"
 #include "attrib/att.h"
-#include "attrib/gattrib.h"
-#include "attrib/gatt_lib.h"
-#include "attio.h"
+#include "gatt.h"
 
-#define SCAN_PARAMETERS_UUID		"00001813-0000-1000-8000-00805f9b34fb"
+#define SCAN_PARAMETERS_UUID128		"00001813-0000-1000-8000-00805f9b34fb"
+#define SCAN_PARAMETERS_UUID		0x1813
 
 #define SCAN_INTERVAL_WIN_UUID		0x2A4F
 #define SCAN_REFRESH_UUID		0x2A31
 
+/*
+ * TODO: Use dynamic value defined by the upper
+ * layer or based on the active profiles.
+ */
 #define SCAN_INTERVAL		0x0060
 #define SCAN_WINDOW		0x0030
+
 #define SERVER_REQUIRES_REFRESH	0x00
 
 struct scan {
-	struct btd_device *device;
-	GAttrib *attrib;
-	struct att_range range;
-	guint attioid;
-	uint16_t interval;
-	uint16_t window;
-	uint16_t iwhandle;
-	uint16_t refresh_handle;
-	guint refresh_cb_id;
+	struct btd_attribute *interval;
+	struct btd_attribute *refresh;
+	unsigned int state_id;
 };
 
-static void write_scan_params(GAttrib *attrib, uint16_t handle)
+static bool refresh_cb(uint8_t *value, size_t len, void *user_data)
 {
-	uint8_t value[4];
+	struct btd_service *service = user_data;
+	struct btd_device *device = btd_service_get_device(service);
+	struct scan *scan = btd_service_get_user_data(service);
+	uint8_t params[4];
 
-	att_put_u16(SCAN_INTERVAL, &value[0]);
-	att_put_u16(SCAN_WINDOW, &value[2]);
+	DBG("Server requires refresh? %d", value[0]);
 
-	gatt_write_cmd(attrib, handle, value, sizeof(value), NULL, NULL);
-}
-
-static void refresh_value_cb(const uint8_t *pdu, uint16_t len,
-						gpointer user_data)
-{
-	struct scan *scan = user_data;
-
-	DBG("Server requires refresh: %d", pdu[3]);
-
-	if (pdu[3] == SERVER_REQUIRES_REFRESH)
-		write_scan_params(scan->attrib, scan->iwhandle);
-}
-
-static void ccc_written_cb(uint8_t status, void *user_data)
-{
-	struct scan *scan = user_data;
-
-	if (status != 0) {
-		error("Write Scan Refresh CCC failed: %s",
-						att_ecode2str(status));
-		return;
-	}
-
-	DBG("Scan Refresh: notification enabled");
-
-	scan->refresh_cb_id = g_attrib_register(scan->attrib,
-				ATT_OP_HANDLE_NOTIFY, scan->refresh_handle,
-				refresh_value_cb, scan, NULL);
-}
-
-static bool discover_descriptor_cb(uint8_t status, GSList *descs,
-								void *user_data)
-{
-	struct scan *scan = user_data;
-	GSList *l;
-
-	if (status == ATT_ECODE_ATTR_NOT_FOUND)
+	if (value[0] != SERVER_REQUIRES_REFRESH)
 		return true;
 
-	if (status != 0) {
-		error("Discover all GATT characteristic descriptors failed: %s",
-							att_ecode2str(status));
-		return true;
-	}
+	att_put_u16(SCAN_INTERVAL, &params[0]);
+	att_put_u16(SCAN_WINDOW, &params[2]);
 
-	for (l = descs; l != NULL; l = g_slist_next(l)) {
-		struct gatt_char_desc *desc = l->data;
-		uint8_t value[2];
-		bt_uuid_t uuid;
-
-		bt_uuid16_create(&uuid, GATT_CLIENT_CHARAC_CFG_UUID);
-		if (bt_uuid_cmp(&desc->uuid, &uuid) != 0)
-			continue;
-
-		att_put_u16(GATT_CLIENT_CHARAC_CFG_NOTIF_BIT, value);
-		gatt_write_char(scan->attrib, desc->handle, 0, value,
-				sizeof(value), ccc_written_cb, user_data);
-	}
+	btd_gatt_write_attribute(device, scan->interval,
+				params, sizeof(params), 0, NULL, NULL);
 
 	return true;
 }
 
-static bool refresh_discovered_cb(uint8_t status, GSList *chars,
-								void *user_data)
-{
-	struct scan *scan = user_data;
-	struct gatt_char *chr;
-	uint16_t start, end;
-
-	if (status) {
-		error("Scan Refresh %s", att_ecode2str(status));
-		return false;
-	}
-
-	if (!chars) {
-		DBG("Scan Refresh not supported");
-		return false;
-	}
-
-	chr = chars->data;
-
-	DBG("Scan Refresh handle: 0x%04x", chr->value_handle);
-
-	start = chr->value_handle + 1;
-	end = scan->range.end;
-
-	if (start > end)
-		return false;
-
-	scan->refresh_handle = chr->value_handle;
-
-	gatt_discover_char_desc(scan->attrib, start, end,
-					discover_descriptor_cb, user_data);
-
-	return true;
-}
-
-static bool iwin_discovered_cb(uint8_t status, GSList *chars, void *user_data)
-{
-	struct scan *scan = user_data;
-	struct gatt_char *chr;
-
-	if (status) {
-		error("Discover Scan Interval Window: %s",
-						att_ecode2str(status));
-		return false;
-	}
-
-	chr = chars->data;
-	scan->iwhandle = chr->value_handle;
-
-	DBG("Scan Interval Window handle: 0x%04x", scan->iwhandle);
-
-	write_scan_params(scan->attrib, scan->iwhandle);
-
-	return true;
-}
-
-static void attio_connected_cb(GAttrib *attrib, gpointer user_data)
-{
-	struct scan *scan = user_data;
-	bt_uuid_t iwin_uuid, refresh_uuid;
-
-	scan->attrib = g_attrib_ref(attrib);
-
-	if (scan->iwhandle) {
-		write_scan_params(scan->attrib, scan->iwhandle);
-		return;
-	}
-
-	bt_uuid16_create(&iwin_uuid, SCAN_INTERVAL_WIN_UUID);
-	bt_uuid16_create(&refresh_uuid, SCAN_REFRESH_UUID);
-
-	gatt_discover_char(scan->attrib, scan->range.start, scan->range.end,
-					&iwin_uuid, iwin_discovered_cb, scan);
-
-	gatt_discover_char(scan->attrib, scan->range.start, scan->range.end,
-				&refresh_uuid, refresh_discovered_cb, scan);
-}
-
-static void attio_disconnected_cb(gpointer user_data)
-{
-	struct scan *scan = user_data;
-
-	g_attrib_unref(scan->attrib);
-	scan->attrib = NULL;
-}
-
-static int scan_register(struct btd_service *service, struct gatt_primary *prim)
+static void state_changed(struct btd_service *service,
+				btd_service_state_t old_state,
+				btd_service_state_t new_state,
+				void *user_data)
 {
 	struct btd_device *device = btd_service_get_device(service);
 	struct scan *scan;
+	uint8_t params[4];
 
-	scan = g_new0(struct scan, 1);
-	scan->device = btd_device_ref(device);
-	scan->range = prim->range;
-	scan->attioid = btd_device_add_attio_callback(device,
-							attio_connected_cb,
-							attio_disconnected_cb,
-							scan);
+	if (service != user_data)
+		return;
 
-	btd_service_set_user_data(service, scan);
+	if (new_state != BTD_SERVICE_STATE_CONNECTED)
+		return;
 
-	return 0;
+	scan = btd_service_get_user_data(service);
+	/* If Refresh exists: write interval when notification arrives */
+	if (scan->refresh && device_is_bonded(device))
+		return;
+
+	att_put_u16(SCAN_INTERVAL, &params[0]);
+	att_put_u16(SCAN_WINDOW, &params[2]);
+
+	DBG("Writting Scan Parameters ...");
+
+	btd_gatt_write_attribute(device, scan->interval,
+			params, sizeof(params), 0, NULL, NULL);
 }
 
 static void scan_param_remove(struct btd_service *service)
 {
 	struct scan *scan = btd_service_get_user_data(service);
 
-	if (scan->attrib != NULL && scan->refresh_cb_id > 0)
-		g_attrib_unregister(scan->attrib, scan->refresh_cb_id);
+	DBG("Removing Scan Parameters");
 
-	btd_device_remove_attio_callback(scan->device, scan->attioid);
-	btd_device_unref(scan->device);
-	g_attrib_unref(scan->attrib);
+	btd_service_remove_state_cb(scan->state_id);
+
 	g_free(scan);
 }
 
 static int scan_param_probe(struct btd_service *service)
 {
 	struct btd_device *device = btd_service_get_device(service);
-	struct gatt_primary *prim;
+	struct btd_attribute *scan_decl, *interval, *refresh;
+	struct scan *scan;
+	GSList *list;
+	bt_uuid_t uuid;
 
 	DBG("Probing Scan Parameters");
 
-	prim = btd_device_get_primary(device, SCAN_PARAMETERS_UUID);
-	if (!prim)
-		return -EINVAL;
+	/* Accessing Scan Parameters Service declaration */
+	bt_uuid16_create(&uuid, SCAN_PARAMETERS_UUID);
+	list = btd_gatt_get_services(device, &uuid);
+	if (list == NULL) {
+		DBG("Scan Parameters: Service missing!");
+		return -1;
+	}
 
-	return scan_register(service, prim);
+	scan_decl = list->data;
+	g_slist_free(list);
+
+	/* Accessing Scan Interval Window declaration */
+	bt_uuid16_create(&uuid, SCAN_INTERVAL_WIN_UUID);
+	list = btd_gatt_get_chars_decl(device, scan_decl, &uuid);
+	if (list == NULL) {
+		DBG("Scan Parameters: Mandatory Interval Window missing!");
+		return -1;
+	}
+
+	/* Accessing Scan Interval Window value */
+	interval = btd_gatt_get_char_value(device, list->data);
+	g_slist_free(list);
+	if (interval == NULL) {
+		DBG("Scan Parameters: Mandatory Interval Window missing!");
+		return -1;
+	}
+
+	/* Accessing Scan Refresh declaration */
+	bt_uuid16_create(&uuid, SCAN_REFRESH_UUID);
+	list = btd_gatt_get_chars_decl(device, scan_decl, &uuid);
+	if (list == NULL) {
+		DBG("Scan Parameters: Refresh (Optional) not supported");
+		return 0;
+	}
+
+	/* Accessing Scan Refresh value */
+	refresh = btd_gatt_get_char_value(device, list->data);
+	g_slist_free(list);
+
+	if (refresh)
+		btd_gatt_add_notifier(refresh, refresh_cb, service);
+
+	scan = g_new0(struct scan, 1);
+	scan->interval = interval;
+	scan->refresh = refresh;
+	scan->state_id = btd_service_add_state_cb(state_changed, service);
+
+	btd_service_set_user_data(service, scan);
+
+	return 0;
 }
 
 static struct btd_profile scan_profile = {
 	.name = "Scan Parameters Client Driver",
-	.remote_uuid = SCAN_PARAMETERS_UUID,
+	.remote_uuid = SCAN_PARAMETERS_UUID128,
 	.device_probe = scan_param_probe,
 	.device_remove = scan_param_remove,
 };
